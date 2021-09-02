1341de850e6ba1d25518c7a234ae036ffb2b6022
Ramon Vitor <vitorramon@hotmail.com>
2021-09-02 18:04:14 -0300

    Requisito 1 - Completo

---
+++ src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Input from '../components/Input';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      playerName: '',
+      // validEmail: false,
+      // validName: false,
+      validation: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onValidation = this.onValidation.bind(this);
+  }
+
+  onValidation() {
+    const min = 3;
+    const { email, playerName } = this.state;
+    const validation = !(/\w+@\w+.com/.test(email)
+    && playerName.length > min
+    && (/[A-z\s]+/).test(playerName));
+    this.setState({ validation });
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.onValidation());
+  }
+
+  render() {
+    const { email, playerName, validation } = this.state;
+    return (
+      <form>
+        <Input
+          labelText="jogador"
+          testid="input-player-name"
+          name="playerName"
+          type="text"
+          value={ playerName }
+          onChange={ this.handleChange }
+        />
+        <Input
+          labelText="email"
+          testid="input-gravatar-email"
+          name="email"
+          type="text"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="btn-play"
+          type="submit"
+          disabled={ validation }
+        >
+          Jogar
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Login;
