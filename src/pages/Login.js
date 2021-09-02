a5de1de8c26f68f1eb40f915dbe208758ec31ba2
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-09-02 18:29:24 -0300

    Conclusao requisito 1

---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabledButton: true,
+      name: '',
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+    this.enableButton();
+  }
+
+  enableButton() {
+    const { name, email } = this.state;
+    if (name.length > 0 && email.length > 0) {
+      this.setState({ disabledButton: false });
+    } else {
+      this.setState({ disabledButton: true });
+    }
+  }
+
+  render() {
+    const { email, name, disabledButton } = this.state;
+    return (
+      <form>
+        <label htmlFor="name">
+          Nome:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            data-testid="input-player-name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            data-testid="input-gravatar-email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ disabledButton }
+        >
+          Jogar
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Login;
---
+++ src/redux/actions/actionType.js
@@ -0,0 +1,3 @@
+export const GET_LOGIN = 'GET_LOGIN';
+
+export const test = 'test';
