4494bff7ac5099d1e96166a9688056c67be8aa22
marcos <marc0sdurval20@gmail.com>
2021-09-02 19:13:04 -0300

    req4finalizado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Game from './pages/Game';
 export default function App() {
   return (
     <BrowserRouter>
-
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
