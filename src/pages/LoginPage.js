6ca2b0ae29254431bce4fcf80fcc8061d710a7e4
Rafael Victor <rafavictorgs@gmail.com>
2021-09-02 19:48:11 -0300

    parte do req 1

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      buttonDisable: true,
+    };
+    this.verifyInputs = this.verifyInputs.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    const { verifyInputs } = this;
+    this.setState({ [name]: value },
+      verifyInputs);
+  }
+
+  handleClick() {
+    const { email, buttonDisable } = this.state;
+    const { history, dispatchInputLogin } = this.props;
+    dispatchInputLogin(email);
+    if (buttonDisable === false) history.push('/carteira');
+  }
+
+  verifyInputs() {
+    const verifyEmail = /.+@.+\.[A-Za-z]+$/;
+    const verifyPassword = 5;
+    const { email, password } = this.state;
+    if (verifyEmail.test(email) && password.length > verifyPassword) {
+      this.setState({ buttonDisable: false });
+    } else { this.setState({ buttonDisable: true }); }
+  }
+
+  render() {
+    const { handleChange, handleClick, state: { buttonDisable } } = this;
+    return (
+      <div className="login-container">
+        <form className="login-form">
+          <h2 className="login-heading">Login Page</h2>
+          <input
+            className="login-nickname"
+            type="text"
+            name="nickname"
+            onChange={ handleChange }
+            placeholder="Nickname"
+            data-testid="input-player-name"
+          />
+          <input
+            className="login-email"
+            type="text"
+            name="email"
+            onChange={ handleChange }
+            placeholder="Email"
+            data-testid="input-gravatar-email"
+          />
+          <button
+            data-testid="btn-play"
+            className="login-btn"
+            type="submit"
+            onClick={ handleClick }
+            disabled={ buttonDisable }
+          >
+            Jogar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatchInputLogin: PropTypes.func.isRequired,
+};
+
+export default LoginPage;
