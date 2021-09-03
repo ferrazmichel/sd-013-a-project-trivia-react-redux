1f23640d2bc59c33159499a9a5bb4da0fe0c38bd
Gabriel Priss <gabrielpriss@hotmail.com>
2021-09-03 18:44:49 -0300

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,12 @@ import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
-    const { header } = this.props;
     return (
       <div>
-        {header.map((element, index) => <p key={ index }>{element}</p>)}
+        <p>oi</p>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  header: state.playerReducer,
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
