7605c5cbefd4d7d88c39e0af5172579b669ed5af
Rafael Victor <rafavictorgs@gmail.com>
2021-09-03 16:32:09 -0300

    Resolve requisito 14

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -5,13 +5,23 @@ import Header from '../components/Header';
 
 class FeedbackPage extends Component {
   render() {
-    const { feedback } = this.props;
+    const { feedback, finalScore, rightAnswers } = this.props;
     const number = 3;
     return (
       <div>
         <Header />
         {(feedback < number) ? <p data-testid="feedback-text">Podia ser melhor...</p>
           : <p data-testid="feedback-text">Mandou bem!</p>}
+        <div>
+          <span data-testid="feedback-total-question">
+            {`right answers: ${rightAnswers}`}
+          </span>
+        </div>
+        <div>
+          <span data-testid="feedback-total-score">
+            {`final score: ${finalScore}`}
+          </span>
+        </div>
       </div>
     );
   }
@@ -23,6 +33,8 @@ FeedbackPage.propTypes = {
 
 const mapStateToProps = (state) => ({
   feedback: state.questions.results,
+  finalScore: state.questions.score,
+  rightAnswers: state.questions.results,
 });
 
 export default connect(mapStateToProps)(FeedbackPage);
