9beb1d2fb136063c583ccfd624e151541861b5ee
Yuri <yurifranco18@live.com>
2021-09-03 16:28:09 -0300

    startamos o req. 8

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Timer from '../components/Timer';
 
 class Game extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <Timer />
+      </div>
     );
   }
 }
