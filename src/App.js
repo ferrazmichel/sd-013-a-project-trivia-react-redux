8ca90b14439b6955648ce31691227e87638b0ef0
João Paulo <jp.viegas@hotmail.com>
2021-09-03 15:33:13 -0300

    iniciando requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Jogo from './pages/Jogo';
 import Login from './pages/Login';
 //
 
 export default function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login */}
+      <Route exact path="/" component={ Login } />
+      <Route path="/jogo" component={ Jogo } />
     </div>
   );
 }
---
+++ src/pages/Jogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Jogo extends React.Component {
+  render() {
+    return (
+      <div>
+        jogo
+      </div>
+    );
+  }
+}
+
+export default Jogo;
