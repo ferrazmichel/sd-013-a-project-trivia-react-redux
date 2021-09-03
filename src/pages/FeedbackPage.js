e3ba27fb0777b5eb853a71b1d8715cbed8287cb9
Rafael Victor <rafavictorgs@gmail.com>
2021-09-03 16:10:33 -0300

    Resolve requisito 12

---
+++ src/pages/FeedbackPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+// import PropTypes from 'prop-types';
+
+class FeedbackPage extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+// FeedbackPage.propTypes = {
+
+// };
+
+export default FeedbackPage;
