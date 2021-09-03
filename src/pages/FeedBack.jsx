5a87b1034ea51c7c6052ec749be6a804270e991e
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-03 19:31:18 -0300

    Fazendo pagina FeedBack

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -4,9 +4,17 @@ import Header from '../components/Header';
 
 class FeedBack extends Component {
   render() {
+    const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
+    const three = 3; // Se for maior que 3 acertos então passa msg
     return (
       <div>
         <Header />
+        <p data-testid="feedback-total-question">{assertions}</p>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-text">
+          {assertions < three ? 'Podia ser melhor...' : 'Mandou bem!'}
+        </p>
+
       </div>);
   }
 }
