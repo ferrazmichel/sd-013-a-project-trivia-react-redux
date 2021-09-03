9d285f7ee1d77acf9765dc90010974bc8a52dd77
Filipe <filipecandido123@hotmail.com>
2021-09-03 17:07:43 -0300

    requisito 15 e 16

---
+++ src/pages/RankingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RankingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Raking</h1>
+      </div>
+    );
+  }
+}
+
+export default RankingPage;
