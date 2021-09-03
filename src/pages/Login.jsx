0b85b90444d414c9c1b0b3a5972d7b192986b352
LuizFJP <luizfjportela@gmail.com>
2021-09-03 15:41:07 -0300

    create index files

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import Input from '../components/Input';
+import { Input } from '../components';
 import { fetchToken, addUser } from '../redux/actions';
 import logo from '../trivia.png';
 
