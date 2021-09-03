2201eaed19747a2c5bb3b9d80905c02e8f16ca51
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-09-03 15:56:45 -0300

    G9: Reqs 13 e 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
+
+const assertionsThreshold = 3;
 
 class Feedback extends Component {
   constructor(props) {
@@ -10,24 +13,43 @@ class Feedback extends Component {
   }
 
   render() {
+    const { score, assertions } = this.props;
+    const msg = assertionsThreshold > assertions ? 'Podia ser melhor...' : 'Mandou bem!';
     return (
-      <div>
-        Feedback Page
-      </div>
+      <>
+        <Header />
+        <main>
+          <div>
+            <h3 data-testid="feedback-text">{msg}</h3>
+            <p>
+              Você fez um total de&nbsp;
+              <span data-testid="feedback-total-score">{score}</span>
+              &nbsp;pontos
+            </p>
+            <p>
+              Acertou&nbsp;
+              <span data-testid="feedback-total-question">{assertions}</span>
+              &nbsp;questões!
+            </p>
+          </div>
+        </main>
+      </>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-
-// });
+const mapStateToProps = (state) => ({
+  score: state.gameReducer.score,
+  assertions: state.gameReducer.assertions,
+});
 
 // const mapDispatchToProps = (dispatch) => ({
 
 // });
 
-export default connect(null, null)(Feedback);
+export default connect(mapStateToProps, null)(Feedback);
 
-// Feedback.propTypes = {
-
-// };
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
