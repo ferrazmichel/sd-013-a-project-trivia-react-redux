0be6847f81fc3df019404adac1118344a833748c
Gustavo Pia <piagja@gmail.com>
2021-09-03 17:58:43 -0300

    criou funçao para mostrar ao usuario a resposta correta atribuindo cores as alternativas

--- src/components/GameQuestion.js
+++ src/components/GameQuestion.js
@@ -8,47 +8,62 @@ class GameQuestion extends Component {
   constructor(props) {
     super(props);
     this.handleQuestion = this.handleQuestion.bind(this);
+    this.dispatchCorrectAnswer = this.dispatchCorrectAnswer.bind(this);
+  }
+
+  dispatchCorrectAnswer() {
+    const { props: { dispatch } } = this;
+    dispatch({ type: 'CORRECT_ANSWER' });
+
+    // adiciona cores a borda das alternativas conforme resposta correta ou errada
+    const correctAnswer = document.querySelector('[data-testid="correct-answer"]');
+    correctAnswer.style.border = '3px solid rgb(6, 240, 15)';
+
+    const wrongAnswers = document.querySelectorAll('[data-testid*="wrong-answer"]');
+    wrongAnswers.forEach((wrongAnswer) => {
+      wrongAnswer.style.border = '3px solid rgb(255, 0, 0)';
+    });
   }
 
   handleQuestion() {
     const { questions } = this.props;
     const currQuestion = questions[index];
     index += 1;
-    if (currQuestion.type === 'multiple') {
-      const currQuestionOptions = [currQuestion.correct_answer,
-        ...currQuestion.incorrect_answers].sort();
-      let wrongIndex = 0;
-      return (
-        <div>
-          <div className="question-n-category">
-            <h3 data-testid="question-category">{currQuestion.category}</h3>
-            <p data-testid="question-text">{currQuestion.question}</p>
-          </div>
-          <div className="question-options">
-            { currQuestionOptions.map((question, questionIndex) => {
-              if (question === currQuestion.correct_answer) {
-                return (
-                  <button
-                    type="button"
-                    data-testid="correct-answer"
-                  >
-                    {question}
-                  </button>);
-              }
-              wrongIndex += 1;
+    const currQuestionOptions = [currQuestion.correct_answer,
+      ...currQuestion.incorrect_answers].sort();
+    let wrongIndex = 0;
+    return (
+      <div>
+        <div className="question-n-category">
+          <h3 data-testid="question-category">{currQuestion.category}</h3>
+          <p data-testid="question-text">{currQuestion.question}</p>
+        </div>
+        <div className="question-options">
+          { currQuestionOptions.map((question, questionIndex) => {
+            if (question === currQuestion.correct_answer) {
               return (
                 <button
                   type="button"
-                  key={ questionIndex }
-                  data-testid={ `wrong-answer-${wrongIndex - 1}` }
+                  data-testid="correct-answer"
+                  onClick={ this.dispatchCorrectAnswer }
                 >
                   {question}
                 </button>);
-            }) }
-          </div>
+            }
+            wrongIndex += 1;
+            return (
+              <button
+                type="button"
+                key={ questionIndex }
+                data-testid={ `wrong-answer-${wrongIndex - 1}` }
+                onClick={ this.dispatchCorrectAnswer }
+              >
+                {question}
+              </button>);
+          }) }
         </div>
-      );
-    }
+      </div>
+    );
   }
 
   render() {
@@ -72,6 +87,7 @@ const mapStateToProps = ({ userReducer }) => ({
 GameQuestion.propTypes = {
   loading: PropTypes.bool.isRequired,
   questions: PropTypes.objectOf().isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps)(GameQuestion);
