2d2940c1ebd0e0056ee4678175b68f356b1a54a8
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-09-03 14:54:41 -0300

    req 4 feito

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const USER_INFO = 'USER_INFO';
+export const PLAYER_INFO = 'PLAYER_INFO';
 
 export const sendUserInfo = (payload) => ({
   type: USER_INFO,
+  payload,
+});
+
+export const sendPlayerInfo = (payload) => ({
+  type: PLAYER_INFO,
   payload,
 });
