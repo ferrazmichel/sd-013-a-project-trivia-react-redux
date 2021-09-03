d84c02f05c06447b969c8ec1fd0a3aee159c35f4
junglejf <jfcchagas@id.uff.br>
2021-09-03 20:48:43 -0300

    Requisito 5: Modificada Funcao submitForm/Logins.js e TokenAPi/services

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { loadFromLocalStaorage } from '../services';
 
 class Play extends Component {
+  componentDidMount() {
+    const token = loadFromLocalStaorage('token');
+    console.log('MONTOU', token);
+  }
+
   render() {
     return (
       <div>
--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,21 @@ export const fetchAPI = () => fetch('https://opentdb.com/api_token.php?command=r
   .then((data) => data.json())
   .then((response) => response);
 
-export const TokenApi = () => {
-  fetchAPI().then(({ token }) => localStorage.setItem('token', token));
+export const TokenApi = () => fetchAPI().then((data) => {
+  localStorage.setItem('token', data.token);
+  return data.response_message;
+});
+
+export const saveToLocalStorage = (key, object) => {
+  localStorage.setItem(key, JSON.stringify(object));
+};
+
+export const loadFromLocalStaorage = (key) => (
+  key === 'token' ? localStorage.getItem(key)
+    : JSON.parse(localStorage.getItem(key)));
+
+export const fetchURL = async (url) => {
+  const requestURL = await fetch(url);
+  const answer = await requestURL.json();
+  return (answer);
 };
