8cb346863d5ac2f48c70b7d367695e720423a346
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-03 23:55:36 -0300

    Corrigindo reducers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { Login, Game, Settings } from './pages/index';
+import { Login, Game, Settings, FeedBack, Ranking } from './pages/index';
 
 export default function App() {
   return (
@@ -9,6 +9,8 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
       <Route exact path="/settings" component={ Settings } />
+      <Route path="/feedback" component={ FeedBack } />
+      <Route path="/ranking" component={ Ranking } />
     </Switch>
   );
 }
