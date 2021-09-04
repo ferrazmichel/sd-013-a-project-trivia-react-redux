6c39588fd6327394c992d46f2fd36d7ee60ac5d7
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-03 23:47:18 -0300

    Corrigindo reducers

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 class Ranking extends Component {
   render() {
     const ranking = JSON.parse(localStorage.getItem('ranking'));
-
     return (
       <div className="ranking">
         <h1 data-testid="ranking-title">Ranking</h1>
