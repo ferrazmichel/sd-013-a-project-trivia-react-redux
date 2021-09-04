6cde0ef7cea66e63adff177a73334914e2128147
Murilo Rainho <murilorainho01@gmail.com>
2021-09-04 12:48:00 -0300

    resolvendo requisito 8

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -9,80 +9,89 @@ class GamePage extends Component {
 
     this.state = {
       numberOfQuestion: 0,
-      allAnswers: [],
-      isLoading: false,
+      counter: 30,
+      crono: 'Timer Function',
+      buttonsDisables: false,
     };
 
     this.submitAnswer = this.submitAnswer.bind(this);
     this.randomAnswer = this.randomAnswer.bind(this);
     this.handleColorChange = this.handleColorChange.bind(this);
+    this.timer = this.timer.bind(this);
   }
 
-  randomAnswer() {
-    const { allAnswers, numberOfQuestion } = this.state;
-    const { questions } = this.props;
-    const index = 4;
-    allAnswers.splice(Math.floor(Math.random() * index),
-      0, questions[numberOfQuestion].correct_answer);
-    this.setState(() => ({ allAnswers, isLoading: false }));
-  }
-
-  submitAnswer() {
-    const { questions } = this.props;
-    this.setState(
-      ({ numberOfQuestion }) => ({
-        isLoading: true,
-        numberOfQuestion: numberOfQuestion + 1,
-        allAnswers: questions[numberOfQuestion + 1].incorrect_answers,
-      }),
-      this.randomAnswer,
-    );
+  componentDidMount() {
+    this.timer();
   }
 
   handleColorChange() {
+    const { crono } = this.state;
     const getBtnsOptions = document.querySelectorAll('.button-answers');
     getBtnsOptions.forEach((button) => {
       if (button.name === 'correct') {
         button.style.border = '3px solid rgb(6, 240, 15)';
       } else { button.style.border = '3px solid rgb(255, 0, 0)'; }
     });
+    clearInterval(crono);
+    this.setState(() => ({ buttonsDisables: true }));
+  }
+
+  timer() {
+    document.querySelectorAll('.button-answers')
+      .forEach((element) => {
+        element.style.border = 'currentColor';
+      });
+    const INTERVAL = 1000;
+    const TIMEOUT = 30000;
+    setTimeout(this.handleColorChange, TIMEOUT);
+    const crono = setInterval(() => {
+      this.setState(({ counter }) => ({ counter: counter - 1 }));
+    }, INTERVAL);
+    this.setState(() => ({ crono }));
+  }
+
+  randomAnswer() {
+    const { numberOfQuestion } = this.state;
+    const { questions } = this.props;
+    const index = 4;
+    const randomizer = [...questions[numberOfQuestion].incorrect_answers];
+    randomizer.splice(Math.floor(Math.random() * index),
+      0, questions[numberOfQuestion].correct_answer);
+    return randomizer;
+  }
+
+  submitAnswer() {
+    this.setState(
+      ({ numberOfQuestion }) => ({
+        numberOfQuestion: numberOfQuestion + 1,
+        counter: 30,
+        buttonsDisables: false,
+      }), this.timer,
+    );
   }
 
   answers() {
     const { questions } = this.props;
-    const { numberOfQuestion, isLoading } = this.state;
-    let { allAnswers } = this.state;
-    if (isLoading) return <span>Loading...</span>;
-    if (!allAnswers.length) {
-      const index = 4;
-      questions[numberOfQuestion].incorrect_answers
-        .splice(Math.floor(Math.random() * index),
-          0, questions[numberOfQuestion].correct_answer);
-      allAnswers = questions[numberOfQuestion].incorrect_answers;
-    }
-    return allAnswers.map((answer, index) => {
+    const { numberOfQuestion, buttonsDisables } = this.state;
+    return this.randomAnswer().map((answer, index) => {
+      let testidButton;
+      let nameButton;
       if (answer === questions[numberOfQuestion].correct_answer) {
-        return (
-          <button
-            data-testid="correct-answer"
-            className="button-answers"
-            name="correct"
-            type="button"
-            key={ index }
-            onClick={ this.handleColorChange }
-          >
-            { answer }
-          </button>
-        );
+        testidButton = 'correct-answer';
+        nameButton = 'correct';
+      } else {
+        testidButton = `wrong-answer-${index}`;
+        nameButton = 'wrong';
       }
       return (
         <button
-          data-testid={ `wrong-answer-${index}` }
           className="button-answers"
-          name="wrong"
-          type="button"
+          data-testid={ testidButton }
+          disabled={ buttonsDisables }
           key={ index }
+          name={ nameButton }
           onClick={ this.handleColorChange }
+          type="button"
         >
           { answer }
         </button>
@@ -92,12 +101,12 @@ class GamePage extends Component {
 
   render() {
     const { questions } = this.props;
-    const { numberOfQuestion } = this.state;
+    const { numberOfQuestion, counter } = this.state;
     return (
       <div>
         <Header />
         <h1>Game Page</h1>
-        <span>temporizador</span>
+        <span>{ counter }</span>
         <h3 data-testid="question-text">{ questions[numberOfQuestion].question }</h3>
         <h4 data-testid="question-category">{ questions[numberOfQuestion].type }</h4>
         { this.answers() }
