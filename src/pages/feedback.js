c7b9106b95adbcc134539ad11d11be2f2f026a2b
Erick Santos <erickosantos.dev@gmail.com>
2021-09-05 21:11:10 -0300

    group-4 - req 12

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
     return (
       <div>
         <p data-testid="feedback-text">Página FeedBack</p>
+        <Header />
       </div>
     );
   }
