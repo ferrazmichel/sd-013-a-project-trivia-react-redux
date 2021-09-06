46c22b7dfff6a5cfeb2bb7f3f51e869873494dd9
Juliane Marques <mqs.juliane@gmail.com>
2021-09-06 18:55:05 -0300

    requisito 13

---
+++ src/pages/FeedbackPage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+import PropTypes from 'prop-types';
+
+class FeedbackPage extends Component {
+    render() {
+      const { feedback } = this.props;
+      const number = 3;
+      return (
+        <div>
+          <Header />
+          {(feedback < number) ? <p data-testid="feedback-text">Podia ser melhor...</p>
+            : <p data-testid="feedback-text">Mandou bem!</p>}
+        </div>
+      );
+    }
+  }
+  
+  FeedbackPage.propTypes = {
+    feedback: PropTypes.func,
+  }.isRequired;
+  
+  const mapStateToProps = (state) => ({
+    feedback: state.questions.results,
+  });
+  
+  export default connect(mapStateToProps)(FeedbackPage);
