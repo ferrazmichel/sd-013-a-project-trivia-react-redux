248e70fe156624c64fad6a5e1933a919ade9914f
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-07 12:18:55 -0300

    Iniciando requisitos 16-18

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,3 +5,18 @@ import { func } from 'prop-types';
 import InputCard from '../components/InputCard';
 import Select from '../components/Select';
 import { actionAddSetting } from '../redux/actions/index';
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    // State inicial vai ser as chaves abaixo
+    this.state = {
+      error: '',
+      categoriesData: [],
+      category: 'General Knowledge',
+      loading: true,
+      amount: 5,
+      difficulty: 'any difficulty',
+      type: 'any type',
+    };
