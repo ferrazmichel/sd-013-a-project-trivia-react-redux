158dee89f4387e1a6f7fef2db727efab43e7bdc8
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-07 12:21:33 -0300

    Iniciando ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,2 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+class Ranking extends Component {
+  render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+
