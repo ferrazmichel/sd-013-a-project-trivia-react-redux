cd45815604ef4894c0201ea6a0ef93bc3055a805
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-07 12:26:57 -0300

    Corrigindo Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { func } from 'prop-types';
-import Input from '../components/InputCard';
+import Input from '../components/Input';
 import Select from '../components/Select';
 import { actionAddSetting } from '../redux/actions/index';
 
