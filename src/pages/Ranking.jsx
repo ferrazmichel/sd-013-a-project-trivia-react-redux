e81c33ac12a355a76b7dd7076276bbb5007a35ec
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-07 12:21:52 -0300

    Estruturando ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -5,3 +5,18 @@ class Ranking extends Component {
   render() {
     const ranking = JSON.parse(localStorage.getItem('ranking'));
 
+    return (
+      <div className="ranking">
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <ul>
+          {ranking.sort((a, b) => b.score - a.score)
+            .map(({ name, score, picture }, index) => (
+              <li key={ name }>
+                <span data-testid={ `player-name-${index}` }>{name}</span>
+                <img src={ picture } alt={ name } />
+                {': '}
+                <span data-testid={ `player-score-${index}` }>{score}</span>
+              </li>
+            ))}
+        </ul>
+
