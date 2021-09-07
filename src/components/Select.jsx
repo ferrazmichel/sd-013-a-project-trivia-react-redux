ccb8275c0cf2235d3722e00f4052294984f164bf
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-07 12:25:49 -0300

    Corrigindo Settings

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,3 +3,21 @@ import React from 'react';
 
 const Select = (props) => {
   const { labelText, id, value, change, name, options } = props;
+
+  return (
+    <label htmlFor={ id }>
+      {labelText}
+      <select
+        id={ id }
+        data-testid={ id }
+        value={ value }
+        onChange={ change }
+        name={ name }
+      >
+        {
+          options.map((option) => <option key={ option }>{option}</option>)
+        }
+      </select>
+    </label>
+  );
+};
