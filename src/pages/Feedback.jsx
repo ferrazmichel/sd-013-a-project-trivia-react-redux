35791691fe3e6e6b8bf044cf5980313fa66077db
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-09-08 14:16:04 -0300

    Adiciona requisito 11

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div data-testid="feedback-text">
+        Página de Feedback
+
+      </div>
+    );
+  }
+}
+
+export default Feedback;
