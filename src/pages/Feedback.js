8806c8129940444674a5541efaabb39f04d20228
Rodrigo Pova <rodrigopova94@gmail.com>
2021-09-08 16:17:09 -0300

    Requisito 13-Implementa mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,38 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Header } from '../components';
 
 // import PropTypes from 'prop-types';
 
 export class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.feedbackMsg = this.feedbackMsg.bind(this);
+  }
+
+  feedbackMsg() {
+    const { score } = this.props;
+    const MIN_SCORE = 3;
+    return score < MIN_SCORE ? 'Podia ser melhor...' : 'Mandou bem!';
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <p data-testid="feedback-text">{ this.feedbackMsg() }</p>
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = ({ player: { score } }) => ({
+  score,
+});
+
+export default connect(mapStateToProps)(Feedback);
