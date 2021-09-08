422432b041be422d8c5781f7f6bf5b7ce74d523f
Rodrigo Pova <rodrigopova94@gmail.com>
2021-09-08 15:54:27 -0300

    Requisito 12- cria rota e renderiza o Header no Feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Header } from '../components';
+
 // import PropTypes from 'prop-types';
 
 export class Feedback extends Component {
   render() {
     return (
       <div>
-        <p>Feedback</p>
+        <Header />
       </div>
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as Game } from './Game';
 export { default as Settings } from './Settings';
+export { default as Feedback } from './Feedback';
