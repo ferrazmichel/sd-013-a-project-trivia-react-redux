67706c1828b500cdfa37f51e783989157bf07d0f
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-09-08 20:04:58 -0300

    Requirement 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Feedback extends Component {
+  feedbackMessage(assertions) {
+    const MINIMUM = 3;
+    if (assertions < MINIMUM) return 'Podia ser melhor...';
+    return 'Mandou bem!';
+  }
+
   render() {
+    const { assertions } = JSON.parse(localStorage.getItem('player'));
+    const message = this.feedbackMessage(assertions);
     return (
       <div>
-        <p data-testid="feedback-text">feedback</p>
+        <Header />
+        <h2 data-testid="feedback-text">{message}</h2>
       </div>
     );
   }
