4d9e2b1929b4884f651fec329228aa3cf4949787
Micael Santos <micaelmaicon@hotmail.com>
2021-09-08 19:24:22 -0300

    Requisito 5

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -1,6 +1,15 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchApiQuestions } from '../redux/actions';
+// import md5 from 'crypto-js/md5';
 
 class Trivia extends Component {
+  componentDidMount() {
+    const { fetchApi } = this.props;
+    fetchApi();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +19,16 @@ class Trivia extends Component {
   }
 }
 
-export default Trivia;
+Trivia.propTypes = {
+  fetchApi: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  info: state.trivia.results,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchApi: () => dispatch(fetchApiQuestions()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Trivia);
