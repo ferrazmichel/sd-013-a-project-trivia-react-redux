709f2ac29b53ea5d19f61f3c1360d9a97d74ae6d
Lucas Santos <lucassantos_dev@outlook.com>
2021-09-08 15:26:24 -0300

    req 18

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h2 data-testid="ranking-title">
-        Texto
-      </h2>
+      <div>
+        <h2 data-testid="ranking-title">
+          Texto
+        </h2>
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">Início</button>
+        </Link>
+      </div>
+
     );
   }
 }
