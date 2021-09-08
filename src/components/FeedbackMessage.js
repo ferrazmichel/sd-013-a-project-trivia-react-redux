89085bed36a2757f4df453b222a239c90f6f6bde
Bruno Pinho <brunopinho@outlook.com>
2021-09-08 18:22:58 -0300

    refatorado requisito 13

---
+++ src/components/FeedbackMessage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const THREE = 3;
+
+class FeedbackMessage extends React.Component {
+  render() {
+    const { player } = this.props;
+    const { assertions } = player;
+
+    return (
+      <div className="feedback-text-box">
+        {
+          assertions >= THREE
+            ? <p data-testid="feedback-text">Mandou bem</p>
+            : <p data-testid="feedback-text">Podia ser melhor...</p>
+        }
+      </div>
+    );
+  }
+}
+
+FeedbackMessage.propTypes = {
+  player: PropTypes.shape({}).isRequired,
+};
+
+export default FeedbackMessage;
