cef9a729324a2152461376d24cccd67492cc813f
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-09-08 15:48:46 -0300

    requisito 03

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 // import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Config from './pages/Config';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/config" component={ Config } />
       </Switch>
     </div>
   );
