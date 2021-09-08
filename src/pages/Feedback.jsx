ce7a7625030f6ed28eead829b7a431322d001bcb
Adriana Barreto <adri92barreto123@gmail.com>
2021-09-08 17:49:10 -0300

    Requesito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class Feedback extends Component {
   render() {
+    const answers = 3;
+    const { assertions } = JSON.parse(localStorage.getItem('state'))
+      .player;
     return (
       <div data-testid="feedback-text">
         Página de Feedback
+        <h1>Feedback</h1>
+        <p data-testid="feedback-text">
+          {assertions < answers ? (
+            <span>Podia ser melhor...</span>
+          ) : (
+            <span>Mandou bem!</span>
+          )}
+        </p>
 
       </div>
     );
