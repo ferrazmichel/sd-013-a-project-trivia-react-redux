8d1ba4ac33694028aa0f44229527c645482a6758
Sethano <vitorcard1720@gmail.com>
2021-09-08 17:45:32 -0300

    requisito 7 feito

--- src/components/GameTrivia.js
+++ src/components/GameTrivia.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Styles/Buttons.css';
 
 class GameTrivia extends React.Component {
+  constructor() {
+    super();
+    this.handleclick = this.handleclick.bind(this);
+  }
+
+  handleclick() {
+    const correct = document.querySelector('#correct');
+    correct.classList.add('buttonCorrect');
+    const incorrect = document.querySelectorAll('#incorrect');
+    incorrect.forEach((e) => {
+      e.classList.add('buttonIncorrect');
+      e.disabled = true;
+    });
+  }
+
   render() {
     const { questions } = this.props;
     return (
@@ -27,6 +43,9 @@ class GameTrivia extends React.Component {
             key={ index }
             type="button"
             data-testid={ `wrong-answer-${index}` }
+            id="incorrect"
+            onClick={ this.handleclick }
+            // className="buttonIncorrect"
           >
             { answer }
           </button>))}
@@ -34,6 +53,9 @@ class GameTrivia extends React.Component {
           key={ questions.correct_answer }
           type="button"
           data-testid="correct-answer"
+          id="correct"
+          onClick={ this.handleclick }
+          // className="buttonCorrect"
         >
           { questions.correct_answer }
         </button>
