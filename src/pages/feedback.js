200155ea3afe61b99ccefbee92ab85dcd27d56df
ALINE <hoshinoaline@yahoo.com.br>
2021-09-08 16:55:10 -0300

    requisito 17

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import md5 from 'crypto-js/md5';
 import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   constructor() {
     super();
     this.message = this.message.bind(this);
+    this.savePlayer = this.savePlayer.bind(this);
+    this.getImage = this.getImage.bind(this);
+  }
+
+  componentDidMount() {
+    this.savePlayer();
+  }
+
+  getImage(email) {
+    const hash = md5(email).toString();
+    return `https://www.gravatar.com/avatar/${hash}`;
+  }
+
+  savePlayer() {
+    const state = JSON.parse(localStorage.getItem('state'));
+    const { player } = state;
+    const { name, score, gravatarEmail } = player;
+    const playerRank = {
+      picture: this.getImage(gravatarEmail),
+      name,
+      score,
+    };
+    if (!localStorage.getItem('ranking')) {
+      localStorage.setItem('ranking', JSON.stringify([playerRank]));
+    } else {
+      const players = JSON.parse(localStorage.getItem('ranking'));
+      players.push(playerRank);
+      localStorage.setItem('ranking', JSON.stringify(players));
+    }
   }
 
   message() {
@@ -27,17 +57,19 @@ class FeedBack extends React.Component {
         <Header />
         {this.message()}
         <p data-testid="feedback-total-score">
-          { player.score }
+          {player.score}
           {' '}
           pontos
         </p>
         <p data-testid="feedback-total-question">
-          { player.assertions }
+          {player.assertions}
           {' '}
           acertos
         </p>
         <Link to="/ranking">
-          <button type="button" data-testid="btn-ranking">Ver Ranking</button>
+          <button type="button" data-testid="btn-ranking">
+            Ver Ranking
+          </button>
         </Link>
         <Link to="/">
           <button data-testid="btn-play-again" type="button">
--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+    this.ranking = this.ranking.bind(this);
+  }
+
+  ranking() {
+    const players = JSON.parse(localStorage.getItem('ranking'));
+    players.sort((a, b) => b.score - a.score);
+    return players;
+  }
+
   render() {
+    const players = this.ranking();
     return (
       <div>
-        <h2 data-testid="ranking-title">
-          Texto
-        </h2>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <ul>
+          {
+            players.map((player, index) => {
+              const { name, picture, score } = player;
+              return (
+                <li key={ index }>
+                  <img
+                    data-testid="header-profile-picture"
+                    src={ picture }
+                    alt={ name }
+                  />
+                  <p>
+                    <span data-testid={ `player-name-${index}` }>{ name }</span>
+                    <span data-testid={ `player-score-${index}` }>{ score }</span>
+                  </p>
+                </li>
+              );
+            })
+          }
+        </ul>
         <Link to="/">
-          <button type="button" data-testid="btn-go-home">Início</button>
+          <button type="button" data-testid="btn-go-home">
+            Início
+          </button>
         </Link>
       </div>
-
     );
   }
 }
