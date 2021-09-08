d322310c2e720edf13bf2366e73b0d942c5f42e2
Gustavo Mauricio <gmauricio207@gmail.com>
2021-09-08 18:11:56 -0300

    Req 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
@@ -13,6 +14,9 @@ export default function App() {
 
       <Route exact path="/game">
         <Game />
+      </Route>
+      <Route exact path="/settings">
+        <Settings />
       </Route>
     </Switch>
   );
