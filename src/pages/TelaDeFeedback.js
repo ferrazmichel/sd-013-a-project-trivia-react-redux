89ac2512ba2592b57891bfd0e4c828e00beab21e
Luiz Casimiro <luizcasimi@gmail.com>
2021-09-09 17:01:43 -0300

    requisito 15 feito

--- src/pages/TelaDeFeedback.js
+++ src/pages/TelaDeFeedback.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class TelaDeFeedback extends Component {
+  constructor() {
+    super();
+
+    this.goToHome = this.goToHome.bind(this);
+  }
+
+  goToHome() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.goToHome }
+        >
+          Jogar Novamente
+        </button>
       </div>
     );
   }
 }
+
+TelaDeFeedback.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
 
 export default TelaDeFeedback;
