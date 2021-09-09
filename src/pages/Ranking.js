3b938301ca4bd45b4438121e5cfc2728fdc0fc56
vdionysio <vini.dionysio@gmail.com>
2021-09-09 15:36:18 -0300

    solving req

---
+++ src/pages/Ranking.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Ranking extends Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <div>
+        Ranking
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
+      </div>
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Ranking;
