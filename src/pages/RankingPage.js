bf5f7889ff8503a9ee78fb83bcbaac277d33ca9d
Filipe <filipecandido123@hotmail.com>
2021-09-09 16:26:22 -0300

    css ranking

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './pagesCSS/rankingpage.css';
 
 class RankingPage extends Component {
   constructor(props) {
@@ -39,7 +40,7 @@ class RankingPage extends Component {
   rankingList() {
     const allPlayers = JSON.parse(localStorage.getItem('ranking'));
     return allPlayers.map(({ name, score, picture }, index) => (
-      <li key={ index }>
+      <li key={ index } className="information-players">
         <img src={ picture } alt="Player gravatar img" />
         <h2 data-testid={ `player-name-${index}` }>{ name }</h2>
         <h2 data-testid={ `player-score-${index}` }>{ score }</h2>
@@ -50,14 +51,17 @@ class RankingPage extends Component {
   render() {
     this.updatingRankingList();
     const allPlayers = JSON.parse(localStorage.getItem('ranking'));
-    const noPlayers = (<span>No Have Players... Go Play Now!!</span>);
+    const noPlayers = (
+      <span className="dont-habe-players">
+        No Have Players... Go Play Now!!
+      </span>);
     return (
-      <div>
-        <h1 data-testid="ranking-title">Raking</h1>
-        <ol>
+      <div className="main-container-ranking">
+        <Link to="/" data-testid="btn-go-home" className="new-start">NEW GAME</Link>
+        <ol className="container-ranking-players">
+          <h1 data-testid="ranking-title">Ranking</h1>
           { (!allPlayers || !allPlayers.length) ? noPlayers : this.rankingList() }
         </ol>
-        <Link to="/" data-testid="btn-go-home">New Start</Link>
       </div>
     );
   }
