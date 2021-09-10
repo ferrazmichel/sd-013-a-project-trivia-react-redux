b6706d4e2405c51c41a24897aa20813e5b53eb60
JeanVPacheco <jeanvitorp@gmail.com>
2021-09-10 18:33:06 -0300

    Adicionou requisito 16

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -75,6 +75,16 @@ class FeedBack extends Component {
             Jogar novamente
           </button>
         </Link>
+        <Link to="/ranking">
+          <button
+            onClick={ () => this.handleClick() }
+            className="btn btn-info"
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }
