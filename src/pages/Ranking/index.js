ccc449f0ce7689d6a777de91837ebbdc325c602d
JeanVPacheco <jeanvitorp@gmail.com>
2021-09-10 18:35:46 -0300

    Adicionou requisito 18

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <div data-testid="ranking-title">
-        Página de Ranking
-      </div>
+      <>
+        <div data-testid="ranking-title">
+          Página de Ranking
+        </div>
+        <Link to="/">
+          <button
+            className="btn btn-info"
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Jogar novamente
+          </button>
+        </Link>
+      </>
     );
   }
 }
