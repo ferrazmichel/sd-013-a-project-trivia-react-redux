cd94a023f80d7d94006a70decf7b6dd9d4a74afe
Breno Lopes <brenokos@gmail.com>
2021-09-10 12:57:10 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConfigScreen, Login, Game, Feedback } from './pages';
 
-
 class App extends React.Component {
   render() {
     return (
