a56d3608c5ef168a1da515e170c48e6efb15dcfc
Diego Vilela <vilelaphp@gmail.com>
2021-09-10 18:28:52 -0300

    Implement the requirement 14

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -6,11 +6,14 @@ class FeedbackPage extends Component {
     const number = 3;
 
     const localState = JSON.parse(localStorage.getItem('state'));
-    const { assertions } = localState.player;
+    const { assertions, score } = localState.player;
 
     return (
       <div>
         <Header />
+        <p data-testid="feedback-total-score">{ score }</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
+
         {(assertions < number) ? <p data-testid="feedback-text">Podia ser melhor...</p>
           : <p data-testid="feedback-text">Mandou bem!</p>}
       </div>
