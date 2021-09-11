da1d4805f0bf8d9029496d112ae1bd55dd07f999
JeanVPacheco <jeanvitorp@gmail.com>
2021-09-11 17:48:00 -0300

    Completando todos os requisitos

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchRanking = this.fetchRanking.bind(this);
+    this.displayRanking = this.displayRanking.bind(this);
+  }
+
+  displayRanking() {
+    const sortedRanking = this.fetchRanking();
+    return sortedRanking.map((player, index) => (
+      <div key={ index }>
+        <h3 data-testid={ `player-name-${index}` }>{ player.name }</h3>
+        <img src={ player.picture } alt={ player.name } />
+        <h4 data-testid={ `player-score-${index}` }>{ player.score }</h4>
+      </div>
+    ));
+  }
+
+  fetchRanking() {
+    const currentRanking = JSON.parse(localStorage.getItem('ranking'));
+    const sortedRanking = currentRanking.sort((a, b) => b.score - a.score);
+    return sortedRanking;
+  }
+
   render() {
+    const ranking = this.displayRanking();
     return (
       <>
         <div data-testid="ranking-title">
-          Página de Ranking
+          <h2>Página de Ranking</h2>
+          { ranking }
         </div>
         <Link to="/">
           <button
