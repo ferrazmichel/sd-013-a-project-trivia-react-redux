e8351a35b061d931b7642c00f5c3d2f6dbe82838
ArthurMGoulart <budiarthur@gmail.com>
2021-09-12 13:59:08 -0300

    Realizando o css da pagina Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ranking.css';
 
 class Ranking extends React.Component {
-  render() {
+  renderRankingList() {
     const rankingStore = localStorage.getItem('ranking');
     const ranking = JSON.parse(rankingStore);
     const sortedRanking = ranking.sort((a, b) => b.score - a.score);
     return (
-      <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <ol>
-          {
-            sortedRanking.map((player, index) => (
-              <li key={ index }>
+      <ol className="list-group list-group-numbered mb-5 ranking-list">
+        {
+          sortedRanking.map((player, index) => (
+            <li
+              key={ index }
+              className="ranking-item
+                list-group-item
+                d-flex justify-content-start align-items-center text-white fw-bold
+                mb-3"
+            >
+              <div className="player-image-name">
                 <img
                   src={ player.picture }
                   alt={ `${player.name} Avatar` }
@@ -20,13 +26,38 @@ class Ranking extends React.Component {
                 />
                 { ' ' }
                 <span data-testid={ `player-name-${index}` }>{ player.name }</span>
-                { ' ' }
+              </div>
+              <div className="player-score">
+                { ' Pontos: ' }
                 <span data-testid={ `player-score-${index}` }>{ player.score }</span>
-              </li>
-            ))
-          }
-        </ol>
-        <Link data-testid="btn-go-home" to="/">Voltar Início</Link>
+              </div>
+            </li>
+          ))
+        }
+      </ol>
+    );
+  }
+
+  render() {
+    return (
+      <div
+        className="text-center
+        d-flex flex-column justify-content-center rounded ranking"
+      >
+        <h1
+          data-testid="ranking-title"
+          className="text-uppercase text-white fw-bold mb-5"
+        >
+          Ranking
+        </h1>
+        { this.renderRankingList() }
+        <Link
+          data-testid="btn-go-home"
+          to="/"
+          className="text-uppercase btn btn-play rounded fw-bold"
+        >
+          Voltar Início
+        </Link>
       </div>
     );
   }
