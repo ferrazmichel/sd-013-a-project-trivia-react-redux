e771bf64a724e0717b6cc8cc955926f560a8975b
Gustavo Mauricio <gmauricio207@gmail.com>
2021-09-14 13:36:30 -0300

    Req 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+
+const qtdAssetions = 3;
 
 class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
   render() {
+    const { score, assertions } = this.props;
+    const message = qtdAssetions > assertions ? 'Podia ser melhor...' : 'Mandou bem!';
     return (
-      <h3 data-testid="feedback-text">Oi</h3>
+      <div>
+        <Header />
+        <h3 data-testid="feedback-text">{ message }</h3>
+        <span
+          data-testid="feedback-total-score"
+        >
+          {score}
+        </span>
+        <span
+          data-testid="feedback-total-question"
+        >
+          {assertions}
+        </span>
+
+      </div>
     );
   }
 }
 
 export default Feedback;
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
