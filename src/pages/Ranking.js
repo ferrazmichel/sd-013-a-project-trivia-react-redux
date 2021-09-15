df4bf687232533e5ab3828f0be524b5a59b26205
CarlosLima <carlos.augustolimaf@gmail.com>
2021-09-15 03:37:23 -0300

    Adiciona o 18 requisito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Muito bem!!</h1>
+      <div>
+        <h1 data-testid="ranking-title">Muito bem!!</h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Jogar novamente
+          </button>
+        </Link>
+      </div>
     );
   }
 }
