db8efa45ef430b2882016798016c61ad97695a4d
João Paulo <jp.viegas@hotmail.com>
2021-09-15 11:43:32 -0300

    pagina config e css

--- src/pages/ConfigPage.jsx
+++ src/pages/ConfigPage.jsx
@@ -5,12 +5,47 @@ import './ConfigPage.css';
 class ConfigPage extends React.Component {
   render() {
     return (
-      <>
-        <h1 className="title" data-testid="settings-title">pagina de configuraçao</h1>
+      <div className="config">
+        <h1 className="title" data-testid="settings-title">Página de configuraçao</h1>
+        <label htmlFor="num">
+          <span>Quantidade de perguntas:</span>
+          <input type="number" name="" id="" min="0" max="10" placeholder="1" />
+        </label>
+
+        <label htmlFor="dif">
+          Dificuldade:
+          <select name="dif" id="dif">
+            <option value="anyDif">Qualquer dificuldade</option>
+            <option value="easy">Fácil</option>
+            <option value="medium" selected>Médio</option>
+            <option value="hard">Difícil</option>
+          </select>
+        </label>
+
+        <label htmlFor="cat">
+          Categoria:
+          <select name="cat" id="cat">
+            <option value="anyCat">Qualquer categoria</option>
+            <option value="music">Música</option>
+            <option value="sport" selected>Esporte</option>
+            <option value="history">História</option>
+            <option value="geral">Conhecimentos gerais</option>
+          </select>
+        </label>
+
+        <label htmlFor="tipo">
+          Tipo:
+          <select name="tipo" id="tipo">
+            <option value="anyType">Qualquer tipo</option>
+            <option value="true">Verdadeiro ou falso</option>
+            <option value="multiple">Multipla escolha</option>
+          </select>
+        </label>
+
         <Link to="/">
           <button className="btn-config" type="button">Voltar</button>
         </Link>
-      </>
+      </div>
     );
   }
 }
